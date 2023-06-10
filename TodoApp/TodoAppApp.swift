cb026614e8ce16d188998a9210d9e3c46b0f9221
Lucas Dalcol Pereira <lucas.pereira_1@philips.com>
2023-06-10 20:08:49 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Lucas Pereira on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
